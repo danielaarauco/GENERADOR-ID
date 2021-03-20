50f1d5219daef0b517257a11312089b57a8b18a0
Daniela Arauco <79011580+danielaarauco@users.noreply.github.com>
2021-03-19 20:23:26 -0400

    subiendo proyecto

---
+++ Solucion_GeneradorDeIdentidades/Generador de identidades/Models/Identity.cs
@@ -0,0 +1,10 @@
+﻿namespace Generador_de_identidades.Models
+{
+    public class Identity
+    {
+        public int IdentityId { get; set; }
+        public string Name { get; set; }
+        public string Photo { get; set; }
+        public string Sex { get; set; }
+    }
+}
